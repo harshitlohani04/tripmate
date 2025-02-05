6efd866170edfb62826cb29a865e1bb1ee5c7c8f
Harshit <kartiklohani2004@gmail.com>
2025-02-05 13:30:20 +0530

    Created django project

--- site/backend/trip_planner/trip_planner/planner/mainPlanner.py
+++ site/backend/trip_planner/trip_planner/planner/mainPlanner.py
@@ -1,17 +1,20 @@
 import google.generativeai as genai
 import json
 import os
+from dotenv import load_dotenv
 
 
 def handleData(file):
     userData = json.load(file)
     def generateResponse():
-        genai.configure(api_key="AIzaSyCmg_nFCCMaYJ6tIeQNB-qPRm8YDS8tdbA")
+        api = os.getenv("Gem_KEY")
+        genai.configure(api_key=api)
         model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content("Explain how AI works")
+        response = model.generate_content(f"Plan an {userData["days"]} day trip to {userData["location"]}")
 
     return generateResponse()
 
 if __name__ == "__main__":
     # response = handleData(file=)
+    load_dotenv()
     print("on work")
